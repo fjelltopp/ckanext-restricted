91049795f3ad8db674c6469b1ef3d9c6d8b94597
Kamil Forenc <kamil.forenc@gmail.com>
2023-01-17 17:40:39 +0100

    Fix lint errors

--- ckanext/restricted/tests/test_performance_package_search.py
+++ ckanext/restricted/tests/test_performance_package_search.py
@@ -12,7 +12,6 @@ import ckanext.restricted.action
 import ckanext.restricted.plugin
 
 
-
 log = logging.getLogger(__name__)
 
 
@@ -20,7 +19,7 @@ log = logging.getLogger(__name__)
 def import_performance_data(clean_db, clean_index):
     raw_db_url = config['sqlalchemy.url']
     sql_file = '/usr/lib/ckan/submodules/ckanext-restricted/ckanext/restricted/tests/performance_test_data.sql'
-    cmd = ["psql",  f"{raw_db_url}", "-f", f"{sql_file}", "> /dev/null"]
+    cmd = ['psql',  f'{raw_db_url}', "-f", f'{sql_file}', '> /dev/null']
 
     log.info(f"Loading performance data using: '{cmd}'")
     subprocess.run(cmd)
