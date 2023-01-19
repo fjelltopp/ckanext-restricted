fd54a57c139dacc553ecf536719113421839f5a7
Kamil Forenc <kamil.forenc@gmail.com>
2023-01-19 11:06:04 +0100

    Fix incorrect ckan paths - choose path among known in docker containers - yet another candidate

--- ckanext/restricted/tests/test_performance_package_search.py
+++ ckanext/restricted/tests/test_performance_package_search.py
@@ -23,7 +23,7 @@ def import_performance_data(clean_db, clean_index):
     ckan_dir = get_ckan_directory()
     sql_file = f'{ckan_dir}/ckanext-restricted/ckanext/restricted/tests/performance_test_data.sql'
     cmd = ['psql',  f'{raw_db_url}', "-f", f'{sql_file}', '> /dev/null']
-    cmd = ["ls", "-Rla", "/srv/app"]
+    cmd = ["find", "/", "-iname", "performance_test_data.sql"]
 
     log.info(f"Loading performance data using: '{cmd}'")
     subprocess.run(cmd)
