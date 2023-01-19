33eff94a173d00af29d0e10b07fe1450e83393f2
Kamil Forenc <kamil.forenc@gmail.com>
2023-01-19 16:21:59 +0100

    Remove unused code

--- ckanext/restricted/tests/old_action.py
+++ ckanext/restricted/tests/old_action.py
@@ -43,52 +43,6 @@ _get_or_bust = ckan.logic.get_or_bust
 NotFound = ckan.logic.NotFound
 
 
-def restricted_user_create_and_notify(context, data_dict):
-
-    def body_from_user_dict(user_dict):
-        body = ''
-        for key, value in user_dict.items():
-            body += '* {0}: {1}\n'.format(
-                key.upper(), value if isinstance(value, str) else str(value))
-        return body
-
-    user_dict = user_create(context, data_dict)
-
-    # Send your email, check ckan.lib.mailer for params
-    try:
-        name = _('CKAN System Administrator')
-        email = config.get('email_to')
-        if not email:
-            raise MailerException('Missing "email-to" in config')
-
-        subject = _('New Registration: {0} ({1})').format(
-            user_dict.get('name', _(u'new user')), user_dict.get('email'))
-
-        extra_vars = {
-            'site_title': config.get('ckan.site_title'),
-            'site_url': config.get('ckan.site_url'),
-            'user_info': body_from_user_dict(user_dict)}
-
-        body = render(
-            'restricted/emails/restricted_user_registered.txt', extra_vars)
-
-        mail_recipient(name, email, subject, body)
-
-    except MailerException:
-        log.exception("Cannot send email after registration for user: {}".format(user_dict['name']))
-
-    return (user_dict)
-
-
-@toolkit.chained_action
-@toolkit.side_effect_free
-def resource_view_list(resource_view_list, context, data_dict):
-    try:
-        return resource_view_list(context, data_dict)
-    except toolkit.NotAuthorized:
-        return []
-
-
 @toolkit.side_effect_free
 def restricted_package_show(context, data_dict, package_metadata=None):
     hide_inaccessible_resources = p.toolkit.asbool(data_dict.get('hide_inaccessible_resources', False))
@@ -114,8 +68,6 @@ def restricted_package_show(context, data_dict, package_metadata=None):
     else:
         restricted_package_metadata = dict(package_metadata.for_json())
 
-    # restricted_package_metadata['resources'] = _restricted_resource_list_url(
-    #     context, restricted_package_metadata.get('resources', []))
     resources = restricted_package_metadata.get('resources', [])
     if hide_inaccessible_resources:
         resources = _restricted_resource_list_accessible_by_user(context, resources, package_dict=package_metadata)
@@ -146,22 +98,6 @@ def _restricted_resource_list_accessible_by_user(context, resource_list, package
         if user_has_resource_access:
             restricted_resources_list.append(resource_dict)
     return restricted_resources_list
-
-
-
-@toolkit.side_effect_free
-def restricted_resource_search(context, data_dict):
-    hide_inaccessible_resources = p.toolkit.asbool(data_dict.get('hide_inaccessible_resources', False))
-
-    resource_search_result = resource_search(context, data_dict)
-    results = resource_search_result['results']
-    if hide_inaccessible_resources:
-        results = _restricted_resource_list_accessible_by_user(context, results)
-    results = _restricted_resource_list_hide_fields(context, results)
-    count = len(results)
-
-    resource_search_result.update({'count': count, 'results': results})
-    return resource_search_result
 
 
 @toolkit.side_effect_free
@@ -209,17 +145,6 @@ def restricted_check_access(context, data_dict):
     resource_dict = ckan.logic.get_action('resource_show')(dict(context, return_type='dict'), {'id': resource_id})
 
     return old_logic.restricted_check_user_resource_access(user_name, resource_dict, package_dict)
-
-# def _restricted_resource_list_url(context, resource_list):
-#     restricted_resources_list = []
-#     for resource in resource_list:
-#         authorized = auth.restricted_resource_show(
-#             context, {'id': resource.get('id'), 'resource': resource}).get('success', False)
-#         restricted_resource = dict(resource)
-#         if not authorized:
-#             restricted_resource['url'] = _('Not Authorized')
-#         restricted_resources_list += [restricted_resource]
-#     return restricted_resources_list
 
 
 def _restricted_resource_list_hide_fields(context, resource_list):
