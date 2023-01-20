fbe7b672ace6594c758091ddb0c789d99ecc36fe
Kamil Forenc <kamil.forenc@gmail.com>
2023-01-20 17:27:27 +0100

    Add migration of ckanext-restricted data from plugin_extra to database

--- ckanext/restricted/migration/restricted/versions/484275223bbe_create_database_model_to_store_.py
+++ ckanext/restricted/migration/restricted/versions/484275223bbe_create_database_model_to_store_.py
@@ -6,11 +6,14 @@ Create Date: 2023-01-17 16:50:23.131955
 
 """
 from alembic import op
+from ckan import model
 from sqlalchemy import ForeignKey, Column, String
 
-
-
 # revision identifiers, used by Alembic.
+from ckan.model import Resource
+from ckan.plugins import toolkit
+from ckanext.restricted import logic
+
 revision = '484275223bbe'
 down_revision = None
 branch_labels = None
@@ -19,6 +22,7 @@ depends_on = None
 
 def upgrade():
     update_schema()
+    migrate_restricted_schema()
 
 
 def update_schema():
@@ -56,3 +60,56 @@ def downgrade():
     op.drop_table('restricted_resource_user_access_control')
     op.drop_table('restricted_resource_org_access_control')
     pass
+
+
+def migrate_restricted_schema():
+    all_resources = model.Session.query(Resource).all()
+
+    for resource in all_resources:
+        resource_dict = resource.as_dict()
+        restricted_data = logic.restricted_get_restricted_dict(resource_dict)
+
+        if resource_has_defined_restrictions(restricted_data):
+            migrate_resource_information(resource_dict, restricted_data)
+
+    model.repo.commit()
+
+
+def migrate_resource_information(resource_dict, restricted_data):
+    from ckanext.restricted.model import ResourceAccessControl
+
+    rac = ResourceAccessControl(resource_id=resource_dict['id'], level='restricted')
+
+    model.Session.add(rac)
+
+    add_user_level_access_if_present(resource_dict, restricted_data)
+    add_org_level_access_if_present(resource_dict, restricted_data)
+
+    model.repo.commit()
+
+
+def add_user_level_access_if_present(resource_dict, restricted_data):
+    from ckanext.restricted.model import ResourceUserAccessControl
+
+    context = {
+        'ignore_auth': True
+    }
+    if restricted_data['allowed_users']:
+        for user in restricted_data['allowed_users']:
+            if user:
+                user_id = toolkit.get_action('user_show')(context, {'id': user})['id']
+                model.Session.add(ResourceUserAccessControl(resource_id=resource_dict['id'], user_id=user_id))
+
+
+def add_org_level_access_if_present(resource_dict, restricted_data):
+    from ckanext.restricted.model import ResourceOrgAccessControl
+
+    if restricted_data['allowed_organizations']:
+        for org in restricted_data['allowed_organizations']:
+            if org:
+                org_id = toolkit.get_action('user_show')({'ignore_auth': True}, {'id': org})['id']
+                model.Session.add(ResourceOrgAccessControl(resource_id=resource_dict['id'], org_id=org_id))
+
+
+def resource_has_defined_restrictions(restricted_data):
+    return restricted_data and restricted_data['level'] and restricted_data['level'] == 'restricted'
