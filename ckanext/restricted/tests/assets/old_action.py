b3be355d3546d2e6d4aa043d3bdb821162ce9ce3
kforenc <114396428+kforenc@users.noreply.github.com>
2023-02-17 11:03:05 +0100

    Adx 939 create performance tests (#80)
    ADX-939 create performance tests
    * Create framework for performance comparison between old and new approach
    * Load database dump with data for performance testing

---
+++ ckanext/restricted/tests/assets/old_action.py
@@ -0,0 +1,189 @@
+# coding: utf8
+
+from __future__ import unicode_literals
+
+import six
+
+import ckan.authz as authz
+from ckan import model
+
+import ckan.lib.base as base
+import ckan.logic
+import ckan.plugins as p
+from ckan.logic.action.get import package_search
+from ckan.logic.action.get import package_show
+from ckan.plugins import toolkit
+
+import ckanext.restricted.tests.assets.old_logic as old_logic
+import json
+
+try:
+    # CKAN 2.7 and later
+    from ckan.common import config
+except ImportError:
+    # CKAN 2.6 and earlier
+    from pylons import config
+
+if six.PY2:
+    render = base.render_jinja2
+else:
+    render = base.render
+
+from logging import getLogger
+log = getLogger(__name__)
+
+
+_get_or_bust = ckan.logic.get_or_bust
+
+NotFound = ckan.logic.NotFound
+
+
+@toolkit.side_effect_free
+def restricted_package_show(context, data_dict, package_metadata=None):
+    hide_inaccessible_resources = p.toolkit.asbool(data_dict.get('hide_inaccessible_resources', False))
+    if not package_metadata:
+        package_metadata = package_show(context, data_dict)
+    else:
+        # context['package'] needs to be set for ckan package auth functions to work
+        # this is set by package_show
+        # if package_show not called, context['package'] needs to be set manually
+        pkg = model.Package.get(package_metadata['id'])
+        if not pkg:
+            raise toolkit.ObjectNotFound(toolkit._('Dataset not found'))
+        context['package'] = pkg
+
+    # Ensure user who can edit can see the resource
+    if authz.is_authorized(
+            'package_update', context, package_metadata).get('success', False):
+        return package_metadata
+
+    # Custom authorization
+    if isinstance(package_metadata, dict):
+        restricted_package_metadata = dict(package_metadata)
+    else:
+        restricted_package_metadata = dict(package_metadata.for_json())
+
+    resources = restricted_package_metadata.get('resources', [])
+    if hide_inaccessible_resources:
+        resources = _restricted_resource_list_accessible_by_user(context, resources, package_dict=package_metadata)
+        restricted_package_metadata['num_resources'] = len(resources)
+    resources = _restricted_resource_list_hide_fields(context, resources)
+    restricted_package_metadata['resources'] = resources
+
+    return (restricted_package_metadata)
+
+
+
+def _restricted_resource_list_accessible_by_user(context, resource_list, package_dict=None):
+    restricted_resources_list = []
+    user_name = old_logic.restricted_get_username_from_context(context)
+    user_obj = context.get('auth_user_obj')
+    for resource in resource_list:
+        resource_dict = dict(resource)
+        if not package_dict:
+            package_dict = package_show(context, {'id': resource_dict['package_id']})
+        user_has_resource_access = old_logic.restricted_check_user_resource_access(
+            user_name,
+            resource_dict,
+            package_dict,
+            user_obj=user_obj,
+            check_access_package_show=False,
+            user_organization_dict=old_logic.get_organization_dict(user_name)
+        ).get('success', False)
+        if user_has_resource_access:
+            restricted_resources_list.append(resource_dict)
+    return restricted_resources_list
+
+
+@toolkit.side_effect_free
+def restricted_package_search(context, data_dict):
+    # pop the param as ckan package search action doesn't support any extra parameters
+    hide_inaccessible_resources = p.toolkit.asbool(data_dict.pop('hide_inaccessible_resources', False))
+    package_search_result = package_search(context, data_dict)
+
+    restricted_package_search_result = {}
+
+    for key, value in package_search_result.items():
+        if key == 'results':
+            restricted_package_search_result_list = []
+            for package in value:
+                restricted_package_search_result_list.append(
+                    restricted_package_show(
+                        context, {'id': package.get('id'), 'hide_inaccessible_resources': hide_inaccessible_resources}, package_metadata=package)
+                )
+            restricted_package_search_result[key] = \
+                restricted_package_search_result_list
+        else:
+            restricted_package_search_result[key] = value
+
+    return restricted_package_search_result
+
+
+@toolkit.side_effect_free
+def restricted_check_access(context, data_dict):
+
+    package_id = data_dict.get('package_id', False)
+    resource_id = data_dict.get('resource_id', False)
+
+    user_name = old_logic.restricted_get_username_from_context(context)
+
+    if not package_id:
+        raise ckan.logic.ValidationError('Missing package_id')
+    if not resource_id:
+        raise ckan.logic.ValidationError('Missing resource_id')
+
+    log.debug("action.restricted_check_access: user_name = " + str(user_name))
+
+    log.debug("checking package " + str(package_id))
+    package_dict = ckan.logic.get_action('package_show')(dict(context, return_type='dict'), {'id': package_id})
+    log.debug("checking resource")
+    resource_dict = ckan.logic.get_action('resource_show')(dict(context, return_type='dict'), {'id': resource_id})
+
+    return old_logic.restricted_check_user_resource_access(user_name, resource_dict, package_dict)
+
+
+def _restricted_resource_list_hide_fields(context, resource_list):
+    restricted_resources_list = []
+    for resource in resource_list:
+        # copy original resource
+        restricted_resource = dict(resource)
+
+        # get the restricted fields
+        restricted_dict = old_logic.restricted_get_restricted_dict(restricted_resource)
+
+        # hide other fields in restricted to everyone but dataset owner(s)
+        if not authz.is_authorized(
+                'package_update', context, {'id': resource.get('package_id')}
+                ).get('success'):
+
+            user_name = old_logic.restricted_get_username_from_context(context)
+
+            # hide partially other allowed user_names (keep own)
+            allowed_users = []
+            for user in restricted_dict.get('allowed_users'):
+                if len(user.strip()) > 0:
+                    if user_name == user:
+                        allowed_users.append(user_name)
+                    else:
+                        allowed_users.append(user[0:3] + '*****' + user[-2:])
+
+            allowed_orgs = []
+            for org in restricted_dict.get('allowed_organizations', []):
+                if len(org.strip()) > 0:
+                    allowed_orgs.append(org)
+
+            new_restricted = json.dumps({
+                'level': restricted_dict.get("level"),
+                'allowed_users': ','.join(allowed_users),
+                'allowed_organizations': ','.join(allowed_orgs)
+            })
+            extras_restricted = resource.get('extras', {}).get('restricted', {})
+            if (extras_restricted):
+                restricted_resource['extras']['restricted'] = new_restricted
+
+            field_restricted_field = resource.get('restricted', {})
+            if (field_restricted_field):
+                restricted_resource['restricted'] = new_restricted
+
+        restricted_resources_list += [restricted_resource]
+    return restricted_resources_list
---
+++ ckanext/restricted/tests/test_performance_package_search.py
@@ -0,0 +1,119 @@
+# encoding: utf-8
+import time
+import os.path
+
+from ckan.tests import helpers
+import logging
+import pytest
+from ckan.common import config
+import subprocess
+
+import ckanext.restricted.action
+import ckanext.restricted.plugin
+import ckanext.restricted.tests.assets.old_action
+
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def import_performance_data(clean_db, clean_index):
+    raw_db_url = config['sqlalchemy.url']
+    ckan_dir = get_ckan_directory()
+    sql_file = f'{ckan_dir}/ckanext/restricted/tests/assets/performance_test_data.sql'
+    cmd = ['psql', f'{raw_db_url}', "-f", f'{sql_file}']
+
+    log.info(f"Loading performance data using: '{cmd}'")
+    completed_process = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    if completed_process.returncode != 0:
+        pytest.fail(f"Couldn't import performance data from file {sql_file}")
+
+    ini_file = f"{ckan_dir}/test.ini"
+
+    cmd = [get_ckan_binary_path(), '-c', ini_file, 'search-index', 'rebuild']
+    log.info(f"Rebuild indexes: {cmd}")
+    subprocess.run(cmd)
+
+
+def get_ckan_directory():
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    dirname = os.path.normpath(current_directory + "../../../../")
+
+    if not os.path.isdir(dirname):
+        raise FileNotFoundError(f"Expected ckanext-restricted sources at '{dirname}' not found")
+
+    return dirname
+
+
+def get_ckan_binary_path():
+    candidates = ['/usr/local/bin/ckan', '/usr/bin/ckan']
+
+    for file_path in candidates:
+        if os.path.isfile(file_path):
+            return file_path
+
+    raise FileNotFoundError("Cannot find ckan binary")
+
+
+@pytest.fixture(autouse=True)
+def add_old_search_action():
+    ckanext.restricted.plugin.RestrictedPlugin.get_actions = Helper.get_actions_with_old_search
+
+
+class Helper:
+    def get_actions_with_old_search(self):
+        return {'user_create': ckanext.restricted.action.restricted_user_create_and_notify,
+                'resource_view_list': ckanext.restricted.action.resource_view_list,
+                'package_show': ckanext.restricted.action.restricted_package_show,
+                'resource_search': ckanext.restricted.action.restricted_resource_search,
+                'package_search': ckanext.restricted.action.restricted_package_search,
+                'restricted_check_access': ckanext.restricted.action.restricted_check_access,
+                'package_search_old': ckanext.restricted.tests.assets.old_action.restricted_package_search}
+
+
+@pytest.mark.usefixtures('add_old_search_action')
+@pytest.mark.usefixtures('clean_db')
+@pytest.mark.usefixtures('clean_index')
+@pytest.mark.ckan_config('ckan.plugins', u'restricted')
+@pytest.mark.usefixtures('with_plugins')
+@pytest.mark.usefixtures('with_request_context')
+@pytest.mark.usefixtures('import_performance_data')
+class TestRestrictedSearchPerformance:
+
+    def test_performance(self):
+        avg_time = self.get_avg_run_time(self._perform_search)
+        old_avg_time = self.get_avg_run_time(self._perform_old_search)
+
+        log.warning(f"Avg time: {avg_time}, old avg time: {old_avg_time}")
+
+    def get_avg_run_time(self, search_function):
+        context = {
+            'ignore_auth': False,
+            'user': 'test_user_00'
+        }
+
+        cumulative_time = 0.0
+        iter_count = 10
+
+        for i in range(0, iter_count):
+            start = time.perf_counter()
+            result = search_function(context)
+            end = time.perf_counter()
+            cumulative_time += end - start
+            assert result['count'] == 299
+
+        return cumulative_time / iter_count
+
+    def _perform_search(self, context):
+        return helpers.call_action(
+            'package_search',
+            context,
+            q="title:Dataset"
+        )
+
+    def _perform_old_search(self, context):
+        return helpers.call_action(
+            'package_search_old',
+            context,
+            q="title:Dataset"
+        )
